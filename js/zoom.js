6f065a960b43421ae1fc8a1a43598772dbd75883
queque <ezekiel.diaz.barrero@gmail.com>
2014-01-16 17:40:23 +0100

    Primeras pruebas: Zoom in and out. Sin posición asignada por defecto.

--- js/zoom.js
+++ js/zoom.js
@@ -1,6 +1,41 @@
 /**
- * http://www.openjs.com/scripts/events/keyboard_shortcuts/
- * Version : 2.01.B
- * By Binny V A 
- * License : BSD 
+ * https://github.com/almaro90/jquery.zoom
+ * Version : 1.00.A
+ * By Almaro90 & Queque 
+ * License : GPL v3 
+ */
+
+/*-----------------------------------------------------------------------------
+ *
+ *				MI ZONA DE TRABAJO, BÚSCATE LA TUYA
+ *
+ *-----------------------------------------------------------------------------
+ */
+ 	function cargarEventos(){
+ 		$("#carga").hide();
+		$(".zoom").click(function(){	
+			$("#carga").find("img").attr("src",$(this).find("img").attr("src"));		
+			$("#carga").toggle();			
+			$( "#carga" ).animate({
+								    width: "100%",
+								    opacity: 1,
+								    marginLeft: "0.6in",
+								    fontSize: "3em",
+								    borderWidth: "10px"
+								  }, 1500 );
+		});
+
+		$("#carga").click(function(){			
+			$("#carga").toggle("slow");
+		});
+
+	}
+
+
+
+/*-----------------------------------------------------------------------------
+ *
+ *							TRABAJA A PARTIR DE AQUÍ
+ *
+ *-----------------------------------------------------------------------------
  */
